3add348f7928f5c136d2850f26d577604ebe17af
Tahera AB <taheraab@gmail.com>
2013-06-05 17:27:01 -0700

    UI: Highlighted active entry and cleaned up navigation list

--- js/main.js
+++ js/main.js
@@ -1,43 +1,25 @@
 var myFeeds = [];
 var myEntries = []; //entries corresponding to a selected feed
-var currentFeedId = 0;
-var currentEntryId = 0;
+var activeFeedId = 0;
+var activeEntryId = 0;
+var $activeEntry = null;
+
 // Create the navigation menu
 $(document).ready(function(){
 	loadFeeds();
-	createCList($("#subsList"));
+	// create navigation list
+	$("#subsList").find("ul").parent().prepend("<span onclick=\"$(this).parent().toggleClass('collapsed');\" ></span> ");
 });
 
-
-//functions to create a collapsible list
-function createCList($list) {
-	$list.find("ul").each(function(){
-		$(this).addClass("noListStyle");
-		$(this).parent().prepend("<span onclick=\"toggleState(this);\" ></span> ");
-		$(this).parent().addClass("expanded");
-	});
-	
-}
-
-function toggleState(elm){
-	console.log("Entered click event handler" + elm.toString());
-	if($(elm).parent().hasClass("collapsed")) {
-		$(elm).parent().removeClass("collapsed");
-		$(elm).parent().addClass("expanded");
-	}else {
-		$(elm).parent().removeClass("expanded");
-		$(elm).parent().addClass("collapsed");
-	}
-}
 
 // Loads feeds from server and populates the navigation list
 function loadFeeds() {
 	$.getJSON("manage_feeds.php?getFeeds", function(feeds) {
 		myFeeds = feeds;
 		var i;
-		var feedList = $("#feedList");
+		var $feedList = $("#feedList");
 		for (i = 0; i < myFeeds.length; i++) {
-			feedList.append("<li><img src = '" + myFeeds[i].alternateLink + "/favicon.ico' width='20px' height='20px'></img>" +
+			$feedList.append("<li><img src = '" + myFeeds[i].alternateLink + "/favicon.ico' width='20px' height='20px'></img>" +
 			" <a href = 'javascript:loadEntries(" + i + ");'>" + myFeeds[i].title + "</a></li>");
 		}
 	});
@@ -46,10 +28,10 @@ function loadFeeds() {
 
 // Load entries for a given feed (index into myFeeds)
 function loadEntries(i) {
-	currentFeedId = myFeeds[i].id;
-	var entryList = $("#entryList");
-	entryList.empty();
-	entryList.append("<h3> <a href='" + myFeeds[i].alternateLink + "'>" + myFeeds[i].title + " >></a></h3><p>" + myFeeds[i].subtitle + "</p><hr>");
+	activeFeedId = myFeeds[i].id;
+	var $entryList = $("#entryList");
+	$entryList.empty();
+	$entryList.append("<h3> <a href='" + myFeeds[i].alternateLink + "'>" + myFeeds[i].title + " >></a></h3><p>" + myFeeds[i].subtitle + "</p><hr>");
 	$.getJSON("manage_feeds.php?getEntries&feedId=" + myFeeds[i].id, function(entries) {
 		myEntries = entries;
 		for (var i = 0; i < myEntries.length; i++) {
@@ -58,14 +40,37 @@ function loadEntries(i) {
 			var updated = new Date(myEntries[i].updated * 1000); // convert unix timestamp into miliseconds
 			content += "<div class='updated'>" + updated.toLocaleString() + "</div></div>";
 			content += "<br /><div>" + myEntries[i].content + "</div></section>";
-			entryList.append(content);
+			$entryList.append(content);
 		}
+		$activeEntry = $("#entryList > section:first");
+		$activeEntry.addClass("highlighted");
+		//Add scroll event handler
+		$entryList.scroll(setActiveEntry);
 	});
 }
 
-// Sets currentEntryId to the entry in focus
-function setCurrentEntryId(id) {
-	currentEntryId = id;
-	console.log("currentFeedId: " + currentFeedId);
-	console.log("currentEntryId: " + currentEntryId);
+// Highlights the current visible entry in the #entryList viewPort and sets current entryId
+function setActiveEntry() {
+	var $viewport = $(this);
+	//Check if current entry is still on top of the viewPort
+	var activeEntryTop = $activeEntry.position().top;
+	var activeEntryBottom = activeEntryTop + $activeEntry.outerHeight(true);
+	var viewportBottom = $viewport.scrollTop() + $viewport.innerHeight();
+	// If active entry has been scrolled up, replace it with next entry
+	if (activeEntryTop < 0) {
+		$activeEntry.toggleClass("highlighted");
+		$activeEntry = $activeEntry.next();
+		$activeEntry.toggleClass("highlighted");
+	}else if (activeEntryBottom > viewportBottom) { // if it has been scrolled down
+		$activeEntry.toggleClass("highlighted");
+		$activeEntry = $activeEntry.prev();
+		$activeEntry.toggleClass("highlighted");
+	}
+	//Check which entry is on the top of the viewPort
+	//var $entries = $("#entryList > section");
+	
 }
+
+
+
+
